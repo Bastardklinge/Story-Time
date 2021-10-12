c9b4bd7f9e90b38b58be886afb3d874d32e1d8b6
Bastardklinge <92378034+Bastardklinge@users.noreply.github.com>
2021-10-12 15:26:12 +0200

    Add files via upload

---
+++ Spieler.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  8 09:09:46 2021
+
+@author: moep
+"""
+import time
+import random
+import os
+class Spieler:
+    
+    Ablauf = ['Zeit','Ort','Person','Tat','Zeuge','Kommentar']
+    
+    #Die Listen öffnen
+    a = open ("Zeit.txt","r")
+    a = a.read()
+    b = open ("Ort.txt","r")
+    b = b.read()
+    c = open ("Person.txt","r")
+    c = c.read()
+    d = open("Tat.txt", "r")
+    d = d.read()
+    e = open("Zeuge.txt","r")
+    e = e.read()
+    f = open("Kommentar.txt","r")
+    f = f.read()
+    
+    
+    #Die Listen einbinden
+    Zeit = a.split("\n")
+    Ort = b.split ("\n")
+    Person = c.split("\n")
+    Tat = d.split("\n")
+    Zeuge = e.split("\n")
+    Kommentar = f.split("\n")
+    
+    #Leere Abschnitte entfernen
+    if "" in Zeit:
+        Zeit.remove("")
+    if "" in Ort:
+        Ort.remove("")
+    if "" in Person:
+        Person.remove("")
+    if "" in Tat:
+        Tat.remove("")
+    if "" in Zeuge:
+        Zeuge.remove("")
+    if "" in Kommentar:
+        Kommentar.remove("")
+    
+    def __init__(self, Name, Status):
+        self.__name = Name
+        self.__status = Status
+        self.__liste = []
+        self.Zeit = []
+        self.Ort = []
+        self.Person = []
+        self.Tat = []
+        self.Zeuge = []
+        self.Kommentar = []
+        #self.__Datei = open(self.__name'.txt', 'w')
+    
+    def Eingabe (self, Teil):
+    
+               
+        if Teil == "Zeit":
+                
+            self.Zeit.append(input("Gib einen Zeitpunkt an: "))
+            f = open(self.__name+'_'+Teil+'.tmp','w')
+            f.write(self.Zeit[0])
+            f.close()
+                
+                        
+        if Teil == "Ort":
+            
+            self.Ort.append(input("Gib einen Ort an: "))
+            f = open(self.__name+'_'+Teil+'.tmp','w')
+            f.write(self.Ort[0])
+            f.close()
+                    
+        if Teil == "Person":
+            
+            self.Person.append(input("Gib eine Person an: "))
+            f = open(self.__name+'_'+Teil+'.tmp','w')
+            f.write(self.Person[0])
+            f.close()
+            
+        if Teil == "Tat":
+            
+            self.Tat.append(input("Gib an, was eine Person getan hat: "))
+            f = open(self.__name+'_'+Teil+'.tmp','w')
+            f.write(self.Tat[0])
+            f.close()
+                        
+        if Teil == "Zeuge":
+            
+            self.Zeuge.append(input("Gib an, wer das ganze bezeugt hat: "))
+            f = open(self.__name+'_'+Teil+'.tmp','w')
+            f.write(self.Zeuge[0])
+            f.close()        
+            
+        if Teil == "Kommentar":
+            
+            self.Kommentar.append(input("Gib an, was zu dem ganzen kommentiert wurde: "))
+            f = open(self.__name+'_'+Teil+'.tmp','w')
+            f.write(self.Kommentar[0])
+            f.close()     
+            
+            
+    def Boteingabe (self, Teil):
+        
+            if Teil == "Zeit":
+                
+                Abschnitt = random.randrange(len(Spieler.Zeit))
+                self.Zeit.append(Spieler.Zeit[Abschnitt])
+                Spieler.Zeit.pop(Abschnitt)
+                f = open(self.__name+'_'+Teil+'.tmp','w')
+                f.write(self.Zeit[0])
+                f.close()
+                
+                        
+            if Teil == "Ort":
+                
+                Abschnitt = random.randrange(len(Spieler.Ort))
+                self.Ort.append(Spieler.Ort[Abschnitt])
+                Spieler.Ort.pop(Abschnitt)
+                f = open(self.__name+'_'+Teil+'.tmp','w')
+                f.write(self.Ort[0])
+                f.close()
+                    
+            if Teil == "Person":
+                
+                Abschnitt = random.randrange(len(Spieler.Person))
+                self.Person.append(Spieler.Person[Abschnitt])
+                Spieler.Person.pop(Abschnitt)
+                f = open(self.__name+'_'+Teil+'.tmp','w')
+                f.write(self.Person[0])
+                f.close()
+                        
+            if Teil == "Tat":
+                
+                Abschnitt = random.randrange(len(Spieler.Tat))
+                self.Tat.append(Spieler.Tat[Abschnitt])
+                Spieler.Tat.pop(Abschnitt)
+                f = open(self.__name+'_'+Teil+'.tmp','w')
+                f.write(self.Tat[0])
+                f.close()
+                        
+            if Teil == "Zeuge":
+                Abschnitt = random.randrange(len(Spieler.Zeuge))
+                self.Zeuge.append(Spieler.Zeuge[Abschnitt])
+                Spieler.Zeuge.pop(Abschnitt)
+                f = open(self.__name+'_'+Teil+'.tmp','w')
+                f.write(self.Zeuge[0])
+                f.close()        
+            
+            if Teil == "Kommentar":
+                
+                Abschnitt = random.randrange(len(Spieler.Kommentar))
+                self.Kommentar.append(Spieler.Kommentar[Abschnitt])
+                Spieler.Kommentar.pop(Abschnitt)
+                f = open(self.__name+'_'+Teil+'.tmp','w')
+                f.write(self.Kommentar[0])
+                f.close()        
+        
+            
+    def Ausgabe (self):
+        
+        
+        Spieler.liste = []
+       
+        for y in Spieler.Ablauf:
+        
+            x = open(self.__name+'_'+y+'.tmp','r')
+            x = x.read()
+            Spieler.liste.append(x)
+            g = open(y+ ".txt","r")
+            g = g.read()
+            g = g.split("\n")
+          
+            if x not in g:
+                f = open (y+".txt","a")
+                f.write("\n"+x)
+                f.close()
+            
+            os.remove(self.__name+"_"+y+".tmp")
+            
+        a = open("Gespeicherte Storys.txt","a")
+        a.write("\n\n"+str(self.__name)+" hat folgende Geschichte: \n\n")
+            
+        print ("\n"+str(self.__name )+ " hat folgende Geschichte: \n\n")
+        time.sleep(1)
+            
+        for x in Spieler.liste:
+            a.write(x + "\n")
+            print (x)
+            time.sleep(1)
+        
+        time.sleep(2)
+        a.close()
---
+++ Story Time.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  8 08:32:43 2021
+
+@author: moep
+"""
+
+import sys
+import random
+import time
+import Spieler
+import names
+
+
+Alphabet = ['a','b','c','d','e','f']
+Ablauf = ['Zeit','Ort','Person','Tat','Zeuge','Kommentar']
+
+
+def Einstellungen():
+    
+    print("Stell zunächst ein, wie viele Spieler und wie viele Bots mitspielen. \n")
+    n = int(input("Wie viele Spieler? "))
+
+
+    for x in range(n):
+        x+=1
+        print('Spieler '+str(x)+", gib hier deinen Namen ein:")
+        Spielerliste.append([input(),"Mensch"])
+
+    m = int(input("Wie viele Bots? "))
+
+    for i in range(m):
+        bots.append([names.get_first_name(),"bot"])
+
+    print("Du spielst mit: \n")
+
+    for i in bots:
+        print(i[0]) 
+    
+        if Spielerliste == []:
+            Spielerliste.append(i)
+    
+        else:
+            Spielerliste.insert(random.randrange(len(Spielerliste)),i)
+    
+    Spiel()
+    
+def Spiel():    
+
+    print(Spielerliste)
+    print("\n")
+    time.sleep(1)
+
+    for x in Spielerliste:
+    
+        if x[1] == "Mensch":
+    
+            print(str(x[0])+' gibt jetzt ein: \n')
+    
+            y = Spielerliste.index(x)   
+            t = 0    
+                
+            while t < 6:
+          
+                if y >= len(Spielerliste):
+                    y -= len(Spielerliste)
+    
+                z = Ablauf[t]
+                f = Spielerliste[y]
+                print(f)
+                g = Spieler.Spieler(f[0],f[1])
+            
+                print(f[0])
+                print(f[1])
+           
+                g.Eingabe(z)
+                y += 1
+                t += 1
+            
+            
+        if x[1] == "bot":
+        
+            print(str(x[0])+' gibt jetzt ein: ')
+    
+            y = Spielerliste.index(x)   
+            t = 0    
+                
+            while t < 6:
+          
+                if y >= len(Spielerliste):
+                    y -= len(Spielerliste)
+    
+                z = Ablauf[t]
+                f = Spielerliste[y]
+                g = Spieler.Spieler(f[0],f[1])
+            
+                g.Boteingabe(z)        
+   
+                y += 1
+                t += 1
+     
+    for x in Spielerliste:
+        z = Spieler.Spieler(x[0],x[1])
+        z.Ausgabe()
+
+    a = int(input("\n Möchtest du... \n 1. ...noch eine Runde spielen? \n 2. ...die Spieler ändern? \n 3. ...beenden? \n\n"))
+    
+    if a == 1:
+        Spiel()
+        
+    if a == 2:
+        Einstellungen()
+
+    if a == 3:
+        sys.exit("Bis dann! ")        
+
+Spielerliste = []
+bots = []
+print("It's Storytime! \n")
+time.sleep(1)
+Einstellungen()
